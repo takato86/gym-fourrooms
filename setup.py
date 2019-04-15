a48880cd20d49d22001105d304d50b87dd740115
tokudo <okudo@datumix.io>
2019-04-15 18:32:41 +0900

    [feat] add setup.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='gym_fourrooms',
+    version='0.0.1',
+    install_requires=['gym'],
+    author='Takato Okudo',
+    author_email='okdtakato@gmail.com',
+)
