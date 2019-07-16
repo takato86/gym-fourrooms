f16f5eab8111a4a5ab020d1a8bce8ec8e4890f17
takato86 <okudo@nii.ac.jp>
2019-07-16 21:24:42 +0900

    test rendering.

--- test/test.py
+++ test/test.py
@@ -4,3 +4,7 @@ import gym_fourrooms
 env = gym.make("ConstFourrooms-v0")
 env = gym.make("ConstOneroom-v0")
 env = gym.make("GoalsOneroom-v0")
+
+env.reset()
+for i in range(1000):
+    env.render()
