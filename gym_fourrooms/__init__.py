2536ab995189aae70014411086d95ca4c1f60f4a
takato86 <okudo@nii.ac.jp>
2020-07-16 21:38:35 +0900

    [bugfix] timelimit to max episode steps.

--- gym_fourrooms/__init__.py
+++ gym_fourrooms/__init__.py
@@ -6,98 +6,98 @@ logger = logging.getLogger(__name__)
 register(
     id='Oneroom-v0',
     entry_point='gym_fourrooms.envs:Oneroom',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id = 'SubGoalFourrooms-v0',
     entry_point = 'gym_fourrooms.envs:SubGoalFourrooms',
-    timestep_limit = 20000,
+    max_episode_steps = 20000,
     reward_threshold = 1
 )
 
 register(
     id='ConstOneroom-v0',
     entry_point='gym_fourrooms.envs:ConstOneroom',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='GoalsOneroom-v0',
     entry_point='gym_fourrooms.envs:GoalsOneroom',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='Tworooms-v0',
     entry_point='gym_fourrooms.envs:Tworooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='Fourrooms-v0',
     entry_point='gym_fourrooms.envs:Fourrooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='Fourrooms-v1',
     entry_point='gym_fourrooms.envs:FourroomsV1',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='FlexibleFourrooms-v0',
     entry_point='gym_fourrooms.envs:FlexibleFourrooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='LargeFourrooms-v1',
     entry_point='gym_fourrooms.envs:LargeFourrooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='ConstFourrooms-v0',
     entry_point='gym_fourrooms.envs:ConstFourrooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='ConstLargeFourrooms-v0',
     entry_point='gym_fourrooms.envs:ConstLargeFourrooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='ShapingFourrooms-v0',
     entry_point='gym_fourrooms.envs:ShapingFourrooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='Threerooms-v0',
     entry_point='gym_fourrooms.envs:Threerooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
 register(
     id='Fiverooms-v0',
     entry_point='gym_fourrooms.envs:Fiverooms',
-    timestep_limit=20000,
+    max_episode_steps=20000,
     reward_threshold=1,
 )
 
